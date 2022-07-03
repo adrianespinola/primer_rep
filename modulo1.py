58a6a39533ca0e8e2165088a191b094cb5bae26c
AdrianEspinola <adrianespinola87@gmail.com>
2022-07-02 21:51:32 -0300

    primer commit

---
+++ modulo1.py
@@ -0,0 +1,4 @@
+def funcionpaquete():
+    print("Estoy en mi paquete")
+
+#carpeta con un .py llamado "__init__" pasa a ser un paquete 
---
+++ modulo2.py
@@ -0,0 +1,13 @@
+def funcion_modulo2():
+    print("acaaaaa")
+
+#git init iniciar un repositorio 
+# git status
+# git add  agregar archivo en la mesita
+
+# ROJO : ARCHIVO MODIFICADO QUE TODAVIA NO SUBI A NINGUNA PARTE DE GIT A NINGUNA "MESITA"
+# COMMIT --> GUARDAR CAMBIOS/VERSIONES EN UNA MESITA
+
+
+
+
